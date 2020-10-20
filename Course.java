fe3ba68e90e1eeeebcd141c49ee865cc1ced948a
bcl-An <72387207+bcl-An@users.noreply.github.com>
2020-10-20 15:24:25 +0800

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,16 +1,25 @@
 public class Course {
     int number;
     String name,place,time,teacher;
+    static String student;
 
     public String toString(){
-        return number+" "+name+" "+place+" "+time+" "+teacher;
+        return "编号："+number+" 课程名称："+name+" 地点："+place+" 时间："+time+" 授课教师："+teacher+" 已选学生："+student;
     }
 
-    Course(int num,String na,String pla,String ti,String te){
+    Course(int num,String na,String pla,String ti){
         number = num;
         name = na;
         place = pla;
         time = ti;
+    }
+
+    void setTeacher(String te){
         teacher = te;
     }
+
+    void setStudent(String stu){
+        student = stu;
+    }
+
 }
