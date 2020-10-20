ebcd9b23d7e51b47f0de31b504c8d9fd05f6b6bf
bcl-An <72387207+bcl-An@users.noreply.github.com>
2020-10-20 15:23:11 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,19 +1,26 @@
 public class Test {
-    void choose(){
-    }
-
-    void drop(){
-    }
-
     public static void main (String[] args){
-        Person per = new Person(1,"a","b");
         Teacher tea1 = new Teacher(12,"刘继兴","男","JAVA");
         Teacher tea2 = new Teacher(24,"曹亚米","女","德语");
-        Student stu1 = new Student(32,"沈柳浩","男","未选");
-        Student stu2 = new Student(16,"赵丽蓓","女","未选");
-        Course cou1 = new Course(2,"JAVA","教215","周一123节","刘继兴");
-        Course cou2 = new Course(6,"德语","教108","周二567节","曹亚米");
+        Student stu1 = new Student(32,"沈柳浩","男");
+        Student stu2 = new Student(16,"赵丽蓓","女");
+        Course cou1 = new Course(1,"JAVA","教215","周一123节");
+        Course cou2 = new Course(2,"德语","教108","周二567节");
+        cou1.setTeacher(tea1.name);
+        cou2.setTeacher(tea2.name);
+        System.out.println("教师：");
+        System.out.println(tea1);
+        System.out.println(tea2);
+        System.out.println("学生：");
+        System.out.println(stu1);
+        System.out.println(stu2);
+        System.out.println("选课：");
+        stu1.choose(cou1.name,stu2.name);
+        stu2.choose(cou2.name,stu1.name);
         System.out.println(cou1);
-        System.out.println(cou1);
+        System.out.println(cou2);
+        System.out.println("退课：");
+        stu1.drop("a","b");
+        System.out.println(stu1);
     }
 }
