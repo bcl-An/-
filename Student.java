badd6734cf8fd64af157ca6cd0a9fd9bce059a86
bcl-An <72387207+bcl-An@users.noreply.github.com>
2020-10-20 15:25:18 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,7 +1,22 @@
 public class Student extends Person{
     String xk;
-    Student(int num,String na,String s,String xk){
+    Course cou;
+
+    Student(int num,String na,String s){
         super(num,na,s);
+    }
+
+    void setXk(String xk){
         this.xk = xk;
+    }
+
+    void choose(String k,String x){
+        cou.setStudent(x);
+        setXk(k);
+    }
+
+    void drop(String a,String b){
+        cou.setStudent("无");
+        System.out.println("姓名："+a+"已退课程："+b);
     }
 }
