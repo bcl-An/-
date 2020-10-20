240e230b250200f73706c5104bacf10c2da57b12
bcl-An <72387207+bcl-An@users.noreply.github.com>
2020-10-20 15:23:50 +0800

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,5 +1,10 @@
 public class Teacher extends Person{
     String sk;
+
+    public String toString(){
+        return "编号："+number+" 姓名："+name+" 性别："+sex+" 教授课程："+sk;
+    }
+
     Teacher(int num,String na,String s,String sk){
         super(num,na,s);
         this.sk = sk;
